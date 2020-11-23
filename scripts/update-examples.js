b4f3bbc7ef93b93e2496d6163cb06159c2aec83f
Daniel Rearden <danielrearden@gmail.com>
2020-11-22 21:43:33 -0500

    chore: update update-examples script

--- scripts/update-examples.js
+++ scripts/update-examples.js
@@ -5,7 +5,7 @@ const { execSync } = require("child_process");
 const examplesDir = path.resolve(__dirname, "../examples");
 const exampleNames = fs.readdirSync(examplesDir);
 for (const exampleName of exampleNames) {
-  execSync("npm install graphql-helix@latest", {
+  execSync("yarn add graphql-helix@latest", {
     cwd: path.join(examplesDir, exampleName),
     stdio: "inherit",
   });
