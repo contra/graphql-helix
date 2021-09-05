f4b0b07038a246891f395355ce35acdc83117848
taneba <taneba.vla@gmail.com>
2021-09-05 10:38:10 +0900

    revert unnecessary change

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,5 +1,6 @@
 export * from "./get-graphql-parameters";
 export * from "./process-request";
+export * from "./render-graphiql";
 export * from "./should-render-graphiql";
 export * from "./types";
 export * from "./errors";
