a52d40b31c5846fa4683b95eb7dbc03e707899a9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-04 07:02:05 -0400

    fix(processRequest): pass context as contextValue correctly (#92)

--- benchmark/k6.js
+++ benchmark/k6.js
@@ -10,7 +10,7 @@ export const options = {
   thresholds: {
     no_errors: ["rate=1.0"],
     expected_result: ["rate=1.0"],
-    http_req_duration: ["avg<=0.6"],
+    http_req_duration: ["avg<=1"],
   },
 };
 
