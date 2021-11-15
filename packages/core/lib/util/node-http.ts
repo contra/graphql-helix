5be7948fc755c494e8e6e1b66cc6ec8ab1c32733
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-15 15:33:46 +0300

    Go for it

--- packages/core/lib/util/node-http.ts
+++ packages/core/lib/util/node-http.ts
@@ -1,23 +1,31 @@
 // @denoify-ignore
 import type { ServerResponse } from "http";
 import type { Http2ServerResponse } from "http2";
+import { isAsyncIterable } from "./is-async-iterable";
 
 export type NodeResponse = ServerResponse | Http2ServerResponse;
 
 export async function sendNodeResponse(responseResult: Response, nodeResponse: NodeResponse): Promise<void> {
   const headersObj: any = {};
   responseResult.headers.forEach((value, name) => {
-    headersObj[name] = value;
+    headersObj[name] = headersObj[name] || [];
+    headersObj[name].push(value);
   });
   nodeResponse.writeHead(responseResult.status, headersObj);
   const responseBody: ReadableStream | null = await responseResult.body;
   if (responseBody == null) {
     throw new Error("Response body is not supported");
   }
-  if ('pipe' in responseBody) {
-    // eslint-disable-next-line no-console
-    console.log('a');
-    (responseBody as any).pipe(nodeResponse);
+  if (responseBody instanceof Uint8Array) {
+    (nodeResponse as any).write(responseBody);
+    nodeResponse.end();
+  } else if (isAsyncIterable(responseBody)) {
+    for await (const chunk of responseBody) {
+      if (chunk) {
+        (nodeResponse as any).write(chunk);
+      }
+    }
+    nodeResponse.end();
   } else if ('getReader' in responseBody) {
     const reader = responseBody.getReader();
     while (true) {
