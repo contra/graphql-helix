1bc05f9d6ca2b23a6af1617bdbdaf6bb401a07ed
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-25 17:46:05 +0300

    Do not use random

--- packages/core/test/utils.test.ts
+++ packages/core/test/utils.test.ts
@@ -1,18 +1,5 @@
 import { calculateByteLength } from "../lib/util/calculate-byte-length";
 
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * The value is no lower than min (or the next integer greater than min
- * if min isn't an integer) and no greater than max (or the next integer
- * lower than max if max isn't an integer).
- * Using Math.round() will give you a non-uniform distribution!
- */
-function getRandomInt(min: number, max: number): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 describe("utils", () => {
     describe("calculateByteLength", () => {
         it('should calculate strings with UTF-8 characters', () => {
@@ -25,13 +12,16 @@ describe("utils", () => {
             const byteLength = calculateByteLength(stringWithEmoji);
             expect(byteLength).toEqual(Buffer.byteLength(stringWithEmoji));
         });
-        it("should calculate byte length of a random string that has both UTF-8 and non UTF-8 characters correctly", () => {
-            let randomString = "";
-            for (let i = 0; i < 25; i++) {
-                randomString += String.fromCharCode(getRandomInt(0, 0xffff));
-            }
-            const byteLength = calculateByteLength(randomString);
-            expect(byteLength).toEqual(Buffer.byteLength(randomString));
+        it("should calculate byte length of a string that has both UTF-8 and non UTF-8 characters correctly", () => {
+            const complexString = [
+                0x7f,
+                0x7ff,
+                0xffff,
+                0xdc00,
+                0xdfff
+            ].map(code => String.fromCharCode(code)).join('A');
+            const byteLength = calculateByteLength(complexString);
+            expect(byteLength).toEqual(Buffer.byteLength(complexString));
         });
     });
 });
