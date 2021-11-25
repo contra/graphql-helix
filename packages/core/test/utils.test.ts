b217db210ae9dd01514337c26a963debe01fb5f9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-25 14:46:54 +0300

    More specific tests

--- packages/core/test/utils.test.ts
+++ packages/core/test/utils.test.ts
@@ -8,20 +8,30 @@ import { calculateByteLength } from "../lib/util/calculate-byte-length";
  * Using Math.round() will give you a non-uniform distribution!
  */
 function getRandomInt(min: number, max: number): number {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 describe("utils", () => {
-  describe("calculateByteLength", () => {
-    const textEncoder = new TextEncoder();
-    it("should calculate byte length of strings that have both UTF-8 and non UTF-8 characters correctly", () => {
-      let randomString = "";
-      for (let i = 0; i < 25; i++) {
-        randomString += String.fromCharCode(getRandomInt(0, 0xffff));
-      }
-      expect(calculateByteLength(randomString)).toEqual(textEncoder.encode(randomString).byteLength);
+    describe("calculateByteLength", () => {
+        it('should calculate strings with UTF-8 characters', () => {
+            const str = "Hello World";
+            const byteLength = calculateByteLength(str);
+            expect(byteLength).toBe(str.length);
+        });
+        it('should calculate strings with emojis correctly', () => {
+            const stringWithEmoji = 'Nice 😋';
+            const byteLength = calculateByteLength(stringWithEmoji);
+            expect(byteLength).toEqual(Buffer.byteLength(stringWithEmoji));
+        });
+        it("should calculate byte length of a random string that has both UTF-8 and non UTF-8 characters correctly", () => {
+            let randomString = "";
+            for (let i = 0; i < 25; i++) {
+                randomString += String.fromCharCode(getRandomInt(0, 0xffff));
+            }
+            const byteLength = calculateByteLength(randomString);
+            expect(byteLength).toEqual(Buffer.byteLength(randomString));
+        });
     });
-  });
 });
