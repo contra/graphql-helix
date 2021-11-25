b00a37b0fb9bf2b5b8b94f1ae4c998662b1cf04b
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-25 12:54:33 +0300

    Use a faster solution

--- packages/core/lib/util/calculate-byte-length.ts
+++ packages/core/lib/util/calculate-byte-length.ts
@@ -1,5 +1,17 @@
+// returns the byte length of an utf8 string
 export function calculateByteLength(str: string): number {
-    const encoder = new TextEncoder();
-    const encoded = encoder.encode(str);
-    return encoded.length;
+  let byteLength = str.length;
+  for (let i = str.length - 1; i >= 0; i--) {
+    const code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) {
+        byteLength++;
+    } else if (code > 0x7ff && code <= 0xffff) {
+        byteLength += 2;
+    }
+    if (code >= 0xdc00 && code <= 0xdfff) {
+        // trail surrogate
+        i--; 
+    }
+  }
+  return byteLength;
 }
