744ab03c128e4918ae56618df9d6c170400fcfb8
Arda TANRIKULU <ardatanrikulu@gmail.com>
2021-11-29 20:54:11 +0300

    Maybe Promise is leaking???

--- benchmark/server.js
+++ benchmark/server.js
@@ -10,27 +10,30 @@ const {
   sendNodeResponse,
 } = require("../packages/core");
 
-const server = createServer(async (req, res) => {
-    const request = await getNodeRequest(req);
+async function handleReqRes(req, res) {
+  const request = await getNodeRequest(req);
 
-    if (shouldRenderGraphiQL(request)) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(renderGraphiQL());
-    } else {
-      const { operationName, query, variables } = await getGraphQLParameters(request);
-      const response = await processRequest({
-        operationName,
-        query,
-        variables,
-        request,
-        schema,
-      });
+  if (shouldRenderGraphiQL(request)) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(renderGraphiQL());
+  } else {
+    const { operationName, query, variables } = await getGraphQLParameters(request);
+    const response = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      schema,
+    });
 
-      await sendNodeResponse(response, res);
-    }
-});
+    await sendNodeResponse(response, res);
+  }
 
-server.listen(5000, "0.0.0.0", () => {
-  // eslint-disable-next-line no-console
-  console.log(`GraphQL Test Server is running... Ready for K6!`);
-});
+  const server = createServer(function (req, res) {
+    handleReqRes(req, res)
+  });
+
+  server.listen(5000, "0.0.0.0", () => {
+    // eslint-disable-next-line no-console
+    console.log(`GraphQL Test Server is running... Ready for K6!`);
+  });
