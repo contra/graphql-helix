f50aa3e811cdb0645a73c597dbb82f34d1a7e5cd
Laurin Quast <laurinquast@googlemail.com>
2021-12-23 18:18:28 +0100

    add failing test

--- packages/core/test/w3c.test.ts
+++ packages/core/test/w3c.test.ts
@@ -1,7 +1,9 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { getGraphQLParameters, processRequest } from "../lib";
+import { getGraphQLParameters, getNodeRequest, processRequest, sendNodeResponse } from "../lib";
 import { stringify as qsStringify } from "qs";
-import { Request } from "cross-undici-fetch";
+import fetch, { Request } from "node-fetch";
+import { createServer, Server } from "http";
+import { PassThrough } from "stream";
 
 const schema = makeExecutableSchema({
   typeDefs: /* GraphQL */ `
@@ -163,5 +165,114 @@ describe("W3 Compatibility", () => {
       -----
       "
     `);
+  });
+});
+
+describe.only("W3 Subscription Node.js", () => {
+  let httpServer: Server;
+  afterEach(async () => {
+    if (httpServer) {
+      await new Promise<void>((resolve) => {
+        httpServer.close(() => resolve());
+      });
+    }
+  });
+  it("calls return on the subscription source if present", async () => {
+    let didCallReturn = false;
+    const source: AsyncGenerator<number> = {
+      async next() {
+        if (didCallReturn === true) {
+          return { value: undefined, done: true };
+        }
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        return { value: 1, done: false };
+      },
+      async return() {
+        didCallReturn = true;
+        return { value: undefined, done: true };
+      },
+      async throw() {
+        throw new Error("NOOP.");
+      },
+      [Symbol.asyncIterator]() {
+        return this;
+      },
+    };
+
+    const schema = makeExecutableSchema({
+      typeDefs: /* GraphQL */ `
+        type Query {
+          hello: String
+        }
+        type Subscription {
+          myNumber: Int
+        }
+      `,
+      resolvers: {
+        Subscription: {
+          myNumber: {
+            subscribe: () => source,
+            resolve: (a) => a,
+          },
+        },
+      },
+    });
+
+    httpServer = createServer(async function handler(req, res) {
+      const request = await getNodeRequest(req);
+
+      const { operationName, query, variables } = await getGraphQLParameters(request);
+
+      const response = await processRequest({
+        operationName,
+        query,
+        variables,
+        request,
+        schema,
+      });
+
+      await sendNodeResponse(response, res);
+    });
+
+    await new Promise<void>((resolve) => {
+      httpServer.listen(8084, () => resolve());
+    });
+
+    const abort = new AbortController();
+    const res = await fetch("http://127.0.0.1:8084/graphql", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        query: /* GraphQL */ `
+          subscription {
+            myNumber
+          }
+        `,
+      }),
+      signal: abort.signal,
+    });
+
+    const response: PassThrough = res.body as any;
+
+    await new Promise<void>((resolve) => {
+      let counter = 0;
+      response.on("data", (d) => {
+        counter++;
+
+        // console.log(String.fromCharCode.apply(null, d));
+
+        if (counter === 3) {
+          abort.abort();
+          // TODO: instead of the timeout we should wait for some kind of end or close event...
+          setTimeout(() => {
+            resolve();
+          }, 300);
+        }
+      });
+    });
+
+    expect(didCallReturn).toEqual(true);
   });
 });
