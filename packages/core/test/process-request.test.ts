be1990bc3059c8dd82d63f6951cff76c2183710d
Daniel Lehr <daniellehr@gmx.de>
2022-03-08 14:50:01 +0100

    adjust test

--- packages/core/test/process-request.test.ts
+++ packages/core/test/process-request.test.ts
@@ -23,7 +23,7 @@ const schema = makeExecutableSchema({
 });
 
 describe("process-request", () => {
-  it("should not allow POST for subscription by default", async () => {
+  it("should not allow POST if disabled", async () => {
     const request = {
       body: { query: "subscription { countdown }" },
       method: "POST",
@@ -39,32 +39,11 @@ describe("process-request", () => {
       variables,
       request,
       schema,
+      allowedSubscriptionHttpMethods: ["GET"],
     });
 
     expect(result.type).toBe("RESPONSE");
     expect((result as any).status).toBe(405);
     expect((result as any).payload.errors[0] instanceof GraphQLError).toBe(true);
-  });
-
-  it("should allow additional allowed HTTP methods for subscriptions", async () => {
-    const request = {
-      body: { query: "subscription { countdown }" },
-      method: "POST",
-      headers: {},
-      query: "",
-    };
-
-    const { operationName, query, variables } = getGraphQLParameters(request);
-
-    const result = await processRequest({
-      operationName,
-      query,
-      variables,
-      request,
-      schema,
-      allowedSubscriptionHttpMethods: ["POST", "GET"],
-    });
-
-    expect(result.type).toBe("PUSH");
   });
 });
